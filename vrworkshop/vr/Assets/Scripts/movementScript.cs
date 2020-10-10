23ec4b634fc84eb830e514cb98a60d1b35d25fd4
chollangimanojkumar <chollangimanojkumar@gmail.com>
2020-10-10 11:46:53 +0530

    all assets

---
+++ vrworkshop/vr/Assets/Scripts/movementScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    public GameObject Sphere;
+    public Texture2D[] images;
+    int imgnum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void next()
+    {
+
+
+        Sphere.GetComponent<Renderer>().material.mainTexture = images[imgnum];
+        imgnum = (imgnum + 1) % images.Length;
+
+
+
+
+    }
+    public void prev()
+    {
+        
+        if (imgnum == 0)
+        {
+            imgnum = images.Length;
+        }
+        else
+        {
+            imgnum = (imgnum - 1) % images.Length;
+        }
+        Sphere.GetComponent<Renderer>().material.mainTexture = images[imgnum];
+
+    }
+}
